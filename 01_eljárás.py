8cc67c7b0fd7dc51fcd0ed85b433da6f15be5124
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2025-01-27 08:46:22 +0100

    Add files

---
+++ 01_eljárás.py
@@ -0,0 +1,8 @@
+
+def koszont_ket_nevvel(nev1, nev2):
+	    print('Szia '+ nev1 + ', ' + nev2 +'!\nÜdv a fedélzeten!')
+
+    nev1__= 'Ádám'
+    nev2__= 'Nóra'
+    koszont_ket_nevvel(nev__2, nev__1)
+    
